7267ab2978361ff104bfad57c858667fd3dc0294
baseplate77 <base8087@gmail.com>
2024-07-24 16:24:59 +0530

    init

---
+++ app/analytics/[id]/page.tsx
@@ -0,0 +1,70 @@
+"use client";
+import Image from "next/image";
+import { useParams } from "next/navigation";
+
+const Analytics = () => {
+  let { id } = useParams();
+
+  return (
+    <div className="flex items-center justify-center bg-[url('/bg_image/bg1.png')] w-screen h-screen bg-no-repeat bg-fixed bg-cover">
+      <div className="border backdrop-blur-lg  rounded-xl py-4 px-8 mx-4">
+        <div className="font-bold text-2xl text-center">
+          Instagram Report for {id}
+        </div>
+        <div className="flex flex-col sm:flex-row gap-8 py-4 ">
+          <div className="flex flex-col gap-4 items-center justify-center">
+            <Image
+              src="https://instagram.fpnq7-6.fna.fbcdn.net/v/t51.2885-19/339863318_798083651202064_3322120079269210626_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.fpnq7-6.fna.fbcdn.net&_nc_cat=104&_nc_ohc=Rc_sMqEgi6IQ7kNvgENcgUR&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AYD4U3SLXY_4fhwhzz2qc6u-1e2LuxFZ4VFJuTvHboGq6A&oe=66A57156&_nc_sid=8b3546"
+              width={150}
+              height={150}
+              className="rounded-full"
+              alt={`${id}`}
+            />
+            <div className="font-bold">@{id}</div>
+          </div>
+          <div>
+            <div className="flex gap-4 justify-between bg-gray/50 border p-2 rounded-md">
+              <div className="text-lg font-bold">Posts : 2,043</div>
+              <div className="text-lg font-bold">Followers : 50K</div>
+            </div>
+            <div className="flex flex-col gap-4 px-6 py-6">
+              <div className="flex justify-between">
+                <Image
+                  src={"/like.svg"}
+                  width={30}
+                  height={30}
+                  alt="average likes"
+                />
+                <div className="font-bold">2000</div>
+              </div>
+              <div className="flex justify-between">
+                <Image
+                  src={"/comment.svg"}
+                  width={30}
+                  height={30}
+                  alt="average comments"
+                />
+                <div className="font-bold">2000</div>
+              </div>
+            </div>
+            <div className="border-b-[1px] border-b-black mb-4" />
+            <div className="flex flex-col gap-2">
+              <div className="flex justify-between items-end">
+                <div className="font-bold">Engagement Rate</div>
+                <div className="text-2xl font-black text-red-400">
+                  {"0.02%"}
+                </div>
+              </div>
+              <div className="flex justify-between items-end">
+                <div className="font-bold">status</div>
+                <div className="font-black text-2xl text-red-400">{"Low"}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Analytics;
